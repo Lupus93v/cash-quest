b2733ba883efa7b2dcbbee307a37ab057e2589fa
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-11-10 18:48:48 +0100

    setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Footer() {
-    
+    return(
+        <div className="footer">
+            <h3>Powered by: Lupus solutions</h3>
+        </div>
+    )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 export default function Header() {
-    
+    return(
+        <div className="header">
+            <NavLink to="about">About</NavLink>
+            <NavLink to="contact">Contact</NavLink>
+        </div>
+    )
 }
