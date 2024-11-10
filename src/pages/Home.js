ec0d6906219fbb4f6eed124f86769e197349668a
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-11-10 20:47:19 +0100

    styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 export default function Home() {
-    return(
+    return (
         <div className="home-page">
-            
+            <div className="navigation">
+                <NavLink className="nav-btn" to="about">About</NavLink>
+                <NavLink className="nav-btn" to="contact">Contact</NavLink>
+            </div>
+            <button className="play-btn">PLAY GAME!</button>
         </div>
     )
 }
