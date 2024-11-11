865547a72156d13422da328fac938146ae383d5b
Lupus93v <161033353+Lupus93v@users.noreply.github.com>
2024-11-11 09:52:28 +0100

    fix2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,15 @@ import React from "react"
 export default function Footer() {
     return(
         <div className="footer">
+            <div>
+                <h3>Something</h3>
+            </div>
+            <div>
+            <h3>Something</h3>
+            </div>
+            <div>
             <h3>Powered by: Lupus solutions</h3>
+            </div>
         </div>
     )
 }
